95d22aa67d0edee16b28ab4cea96a626e531b50f
priyanshsingh <priyanshsingh1401@gmail.com>
2022-05-08 14:58:32 +0530

    props during type added sucessfully

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import GoogleButton from 'react-google-button';
 import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
 
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 
 export default function BasicTextFields() {
+
+    const [user, setUser] = useState({
+        firstName: "", lastName: "", email: "", username: "", password: ""
+    })
+
+    let name, value;
+
+    const handleInputs = (e) => {
+        console.log(e);
+        name = e.target.name;
+        value = e.target.value;
+
+        setUser({ ...user, [name]: value });
+    }
+
+    // const PostData = async (e) => {
+    //     e.preventDefault();
+    //     const{firstName, lastName, email, username, password} = user;
+    // }
+
     return (
 
         <>
@@ -28,36 +51,74 @@ export default function BasicTextFields() {
                     borderRadius: '10px'
                 }}
             >
-                <Typography variant="h3" color="grey" style={{ marginTop: '10px', marginBottom: '10px' }}>Register Now</Typography>
-                <TextField className="text_field_email" required label="FirstName" style={{ margin: '10px 0', width: '80%' }} />
-                <TextField className="text_field_email" required label="FirstName" style={{ margin: '10px 0', width: '80%' }} />
-                <TextField className="text_field_email" required label="E-Mail" style={{ margin: '10px 0', width: '80%' }} />
-                <TextField className="text_field_email" required label="UserName" style={{ margin: '10px 0', width: '80%' }} />
+                <Typography
+                    variant="h3"
+                    color="grey"
+                    style={{ marginTop: '10px', marginBottom: '10px' }}>Register Now</Typography>
+                <form method="POST">
+                    <TextField
+                        value={user.firstname}
+                        onChange={handleInputs}
+                        name='firstname'
+                        className="text_field_email"
+                        required label="FirstName"
+                        style={{ margin: '10px 0', width: '80%' }} />
 
-                <TextField
-                    className="text_field_pass"
-                    required
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    style={{ margin: '10px 0', width: '80%' }}
-                />
+                    <TextField
+                        value={user.lastname}
+                        onChange={handleInputs}
+                        name='lastname'
+                        className="text_field_email"
+                        required label="LastName"
+                        style={{ margin: '10px 0', width: '80%' }} />
+
+                    <TextField
+                        value={user.email}
+                        onChange={handleInputs}
+                        name='email'
+                        className="text_field_email"
+                        required label="E-Mail"
+                        style={{ margin: '10px 0', width: '80%' }} />
+
+                    <TextField
+                        value={user.username}
+                        onChange={handleInputs}
+                        name='username'
+                        className="text_field_email"
+                        required label="UserName"
+                        style={{ margin: '10px 0', width: '80%' }} />
+
+                    <TextField
+                        className="text_field_pass"
+                        required
+                        value={user.password}
+                        onChange={handleInputs}
+                        name='password'
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        style={{ margin: '10px 0', width: '80%' }}
+                    />
+                </form>
                 <br />
-                <Button variant="contained" size="large" style={{fontSize:'1.3rem', marginTop:'10px', marginBottom:'10px'}}>
+                <Checkbox {...label} defaultChecked color="success" />
+                I agree the privacy policy of the company
+                <br />
+                <Button variant="contained" size="large" style={{ fontSize: '1.3rem', marginTop: '10px', marginBottom: '10px' }}>
                     Submit
                 </Button>
                 <hr style={{
-                    height: '30px', 
-                    borderStyle: 'dotted', 
-                    borderColor: '#8c8b8b', 
-                    borderWidth: '1px 0 0 0', 
+                    height: '30px',
+                    borderStyle: 'dotted',
+                    borderColor: '#8c8b8b',
+                    borderWidth: '1px 0 0 0',
                     borderRadius: '20px'
                 }} />
                 <p className="or">OR</p>
                 <GoogleButton style={{ marginLeft: '80px' }} />
             </Box>
-                <br />
-                <br />
+            <br />
+            <br />
         </>
     );
 }
