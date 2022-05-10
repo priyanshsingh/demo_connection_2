c3fce9297fb1f378ddfdfdde590daffeebb14ba1
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-05-11 00:32:04 +0530

    started getting array of blogs

--- server/routes/home.js
+++ server/routes/home.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose')
 const User = mongoose.model('User')
 
 router.use('/', utils.authMiddleware ,async (req, res, next)=>{
-    await User.find({})
+    let arr = []
+    let arr2 = []
+await User.find({}, {blogs: {_id: 0}, _id: 0, firstName: 0, lastName: 0, email: 0, hash: 0, salt: 0, username: 0, __v: 0})
     .then(data=>{
-        return res.status(200).json(data)
+        return res.status(200).json(data[0].blogs)
     })
     .catch(err=>{
         res.status(401).json({message: "unable to load data"})
